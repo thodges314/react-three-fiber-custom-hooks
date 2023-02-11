963128eb76e6cf1d5352d1c2cc58850ea3122e84
Thomas Hodges <thodges@javascriptonline.org>
2023-02-10 17:17:29 -0800

    keymaps and stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+import { OrbitControls, Stats } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import Box from "./Box";
-import { Stats, OrbitControls } from "@react-three/drei";
+import useKeyboard from "./useKeyboard";
 
-const App = () => (
-  <Canvas camera={{ position: [1, 2, 3] }}>
-    <Box position={[0, 0.5, 0]} />
-    <OrbitControls />
-    <axesHelper args={[5]} />
-    <gridHelper />
-    <Stats />
-  </Canvas>
-);
+const App = () => {
+  const keyMap = useKeyboard();
+  return (
+    <Canvas camera={{ position: [1, 2, 3] }}>
+      <Box position={[-1.5, 0.5, 0]} keyMap={keyMap} />
+      <Box position={[0, 0.5, 0]} keyMap={keyMap} selected />
+      <Box position={[1.5, 0.5, 0]} keyMap={keyMap} />
+      <OrbitControls />
+      <axesHelper args={[5]} />
+      <gridHelper />
+      <Stats />
+    </Canvas>
+  );
+};
 
 export default App;
--- src/Box.jsx
+++ src/Box.jsx
@@ -1,23 +1,22 @@
 import { useRef, useState } from "react";
 import { useFrame } from "@react-three/fiber";
-import useKeyboard from "./useKeyboard";
 
 const Box = (props) => {
   const ref = useRef();
-  const keyMap = useKeyboard();
-  // const [selected, setSelected] = useState(false);
+  const keyMap = props.keyMap;
+  const [selected, setSelected] = useState(props.selected);
 
   useFrame((_, delta) => {
-    keyMap["KeyA"] && (ref.current.position.x -= 1 * delta);
-    keyMap["KeyD"] && (ref.current.position.x += 1 * delta);
-    keyMap["KeyW"] && (ref.current.position.z -= 1 * delta);
-    keyMap["KeyS"] && (ref.current.position.z += 1 * delta);
+    keyMap["KeyA"] && selected && (ref.current.position.x -= 1 * delta);
+    keyMap["KeyD"] && selected && (ref.current.position.x += 1 * delta);
+    keyMap["KeyW"] && selected && (ref.current.position.z -= 1 * delta);
+    keyMap["KeyS"] && selected && (ref.current.position.z += 1 * delta);
   });
 
   return (
     <mesh ref={ref} {...props} onPointerDown={() => setSelected(!selected)}>
       <boxGeometry />
-      <meshBasicMaterial color={0x00ff00} wireframe />
+      <meshBasicMaterial color={0x00ff00} wireframe={!selected} />
     </mesh>
   );
 };
